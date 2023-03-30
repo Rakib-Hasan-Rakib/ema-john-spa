c0f408c24e61249fedf6bd95bdb8ef05a00a4272
unknown <rakibhasanrakib3006@gmail.com>
2023-03-30 17:38:23 +0600

    shop created

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,12 @@
 import React from 'react';
+import logo from '../../../public/images/Logo.svg'
 
 const Header = () => {
     return (
       <div className="bg-neutral">
         <div className="navbar w-11/12 mx-auto">
           <div className="flex-1">
-            <img
-              className="w-[25%]"
-              src="../../../public/images/Logo.svg"
-              alt="nav logo"
-            />
+            <img className="w-[25%]" src={logo} alt="nav logo" />
           </div>
           <div className="flex-none">
             <ul className="menu menu-horizontal px-1 text-white">
